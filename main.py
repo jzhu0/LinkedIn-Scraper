cb64578f9af55278a225e0688c0338aaadae358e
Jason Zhu <jasonzhu@cs.washington.edu>
2017-07-21 22:21:53 -0700

    find elements not working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Created on Jul 21, 2017
 '''
 DATASET_NAME = "COPY - Founder backgrounds & Operational - 7.5.2017.xlsx"
 DATASET_SHEETNAME = "Sheet1"
-DOWNLOAD_DIRECTORY = ""
+DOWNLOAD_DIRECTORY = "C:/Users/LinkedIn profiles"
 
 import time
 from bs4 import BeautifulSoup
@@ -16,7 +16,8 @@ from selenium import webdriver
 
 # Extraction functions
 def download_profile(driver):
-    driver.find_element_by_class_name("action-name-btn").click()
+    driver.find_element_by_class_name("pv-top-card-overflow__trigger").click()
+    driver.find_element_by_css_selector("button.action-name-btn").click()
 
 print "Loading data set " + DATASET_NAME + "..."
 BgOpData = openpyxl.load_workbook(DATASET_NAME)
@@ -27,28 +28,32 @@ else:
     raise Exception("Data set did not contain correct data sheet")
 print "    Data set successfully opened"
 for row in BgOpDataSheet.iter_rows():
-    if (row[0].value == "Project ID"):   # skip the first row
+    if (str(row[0].value) == "Project ID"):   # skip the first row
         continue
-    if not (row[2].value == "None"):   # skip rows without a link
-        links[row[1].value] = str(row[2].value)
-        print "Loaded pID #" + str(row[0].value) + ": " + str(row[1].value)
+    if not (str(row[2].value) == "None" or str(row[2].value) == ""):   # skip rows without a link
+        links[str(row[1].value)] = str(row[2].value)
+        print "Loaded pID #" + str(row[0].value) + ": " + str(row[1].value) + " " + str(row[2].value)
     if (row[0].value > 10):   # test loading a few rows
         break
 print "    Data set successfully loaded"
+print str(links)
 
 # test with smaller links dict:
-print "Using hard-coded data set."
-links = {"Raanan Zehavi" : "https://www.linkedin.com/in/raanan-zehavi-418933b/",
-"Aksel Chernitzky" : "https://www.linkedin.com/in/aksel-chernitzky-5902a/",
-"Janice Chernitzky" : "https://www.linkedin.com/in/janice-chernitzky-7259b763/",
-"Daniel Ferrazzoli" : "https://www.linkedin.com/in/daniel-ferrazzoli-2a76a72a/"}
-
+#print "Using hard-coded data set."
+#links = {"Raanan Zehavi" : "https://www.linkedin.com/in/raanan-zehavi-418933b/",
+#"Aksel Chernitzky" : "https://www.linkedin.com/in/aksel-chernitzky-5902a/",
+#"Janice Chernitzky" : "https://www.linkedin.com/in/janice-chernitzky-7259b763/",
+#"Daniel Ferrazzoli" : "https://www.linkedin.com/in/daniel-ferrazzoli-2a76a72a/"}
 
 # Set download location preferences
 preferences = {"download.default_directory" : DOWNLOAD_DIRECTORY}
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", preferences)
 driver = webdriver.Chrome(chrome_options = options)
+
+# navigate to LinkedIn homepage - log in manually
+driver.get("https://www.linkedin.com/")
+time.sleep(30)
 
 for name, pURL in links.iteritems():
     print "Processing " + name
